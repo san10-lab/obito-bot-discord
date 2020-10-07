b97da8a85e97864f32f8428eede94980567bf109
san10-lab <72431516+san10-lab@users.noreply.github.com>
2020-10-07 04:04:04 -0300

    Create server.js
    Comando do server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const Dlang = require('discordbot-script')
+const discordScript = require("discordbot-script")
+
+const bot = new discordScript({
+  token: "....",
+  prefix: ["..."]
+})
+
+bot.variables({
+ aspas1: "1"
+ aspas2: "2"
+Etc...
+ })
+
